5e4a7e72abfa174d64db1a2fbab317bfa1888440
BcodezMUJJE <billbrightmatege@gmail.com>
2023-09-18 05:27:00 -0700

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
 
+""" The State Module for HBNB project """
 
-class Amenity(BaseModel):
-    name = ""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+class Amenity(BaseModel, Base):
+    """The amenity class, contains name field and places relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column('name', String(128), nullable=False)
