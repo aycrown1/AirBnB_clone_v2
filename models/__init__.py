be278c8cc568a093264c0c87dae89c17ad8f8033
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-18 11:39:13 +0000

    Storage type Improvement

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""Instantiates a storage objects
+Creating an instance of DBStorage and store it in the variable storage
+Creating an instance of FileStorage and store it in the variable storage
+if the value of the environment variable is db,
+then we use SQL database, else JSON file as storage.
+"""
+from os import getenv
 
 
-storage = FileStorage()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
