968cc0d39fa5d2dd25135e7e4c41c70b039b8185
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-23 09:27:34 +0000

    corection2

--- models/place.py
+++ models/place.py
@@ -17,48 +17,36 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
-        __tablename__ = "places"
-        city_id = Column('city_id',
-                         String(60), ForeignKey('cities.id'),
-                         nullable=False)
+    __tablename__ = "places"
 
-        user_id = Column('user_id',
-                         String(60), ForeignKey('users.id'),
-                         nullable=False)
+    city_id = Column('city_id',
+                     String(60), ForeignKey('cities.id'),
+                     nullable=False)
 
-        name = Column("name", String(128), nullable=False)
-        description = Column("description", String(1024))
-        number_rooms = Column("number_rooms", Integer(),
+    user_id = Column('user_id',
+                     String(60), ForeignKey('users.id'),
+                     nullable=False)
+
+    name = Column("name", String(128), nullable=False)
+    description = Column("description", String(1024))
+    number_rooms = Column("number_rooms", Integer(),
+                          nullable=False, default=0)
+
+    number_bathrooms = Column("number_bathrooms", Integer(),
                               nullable=False, default=0)
 
-        number_bathrooms = Column("number_bathrooms", Integer(),
-                                  nullable=False, default=0)
-
-        max_guest = Column("max_guest", Integer(),
-                           nullable=False, default=0)
-        price_by_night = Column(
-            "price_by_night", Integer(), nullable=False, default=0)
-        latitude = Column("latitude", Float())
-        longitude = Column("longitude", Float())
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete, delete-orphan")
-        amenity_ids = []
-        # many to many Place<->Amenity
-        amenities = relationship('Amenity', secondary=place_amenity,
-                                 backref="places", viewonly=False)
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+    max_guest = Column("max_guest", Integer(),
+                       nullable=False, default=0)
+    price_by_night = Column(
+        "price_by_night", Integer(), nullable=False, default=0)
+    latitude = Column("latitude", Float())
+    longitude = Column("longitude", Float())
+    reviews = relationship("Review", backref="place",
+                           cascade="all, delete, delete-orphan")
+    amenity_ids = []
+    # many to many Place<->Amenity
+    amenities = relationship('Amenity', secondary=place_amenity,
+                             backref="places", viewonly=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') != "db":
         @property
