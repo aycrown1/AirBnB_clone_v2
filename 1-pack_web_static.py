12d51bf447a025760a0fd902f4ce39a694f0e267
aycrown1 <ayoadeadedamola198@gmail.com>
2023-10-05 10:52:23 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,21 +6,15 @@ import os
 from datetime import datetime
 from fabric.api import local, runs_once
 
-@runs_once
 
+@runs_once
 def do_pack():
     """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+    output = "versions/web_static_{}.tgz".format(
+        now.strftime("%Y%m%d%H%M%S"))
     try:
         print("Packing web_static to {}".format(output))
         local("tar -cvzf {} web_static".format(output))
