4e96494377a224fafd56eb1afaada59d350bb17b
aycrown1 <ayoadeadedamola198@gmail.com>
2023-10-22 16:16:02 +0100

    another

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/data/data/com.termux/files/home/aycrown1/AirBnB_clone_v2/flask"
+set -gx VIRTUAL_ENV "/root/AirBnB_clone_v2/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
